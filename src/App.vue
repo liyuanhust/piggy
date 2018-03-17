8fa016617059b12aa05db8ba74eaf70f7efa46b1
Jiayang Yan <raymond.dream@gmail.com>
2018-03-17 21:22:56 +0800

    [YJY] add form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <md-tabs md-sync-route>
+      <md-tab id="tab-home" md-label="Home" to="/components/tabs/home"></md-tab>
+      <md-tab id="tab-pages" md-label="Pages" to="/components/tabs/pages"></md-tab>
+      <md-tab id="tab-posts" md-label="Posts" to="/components/tabs/posts"></md-tab>
+      <md-tab id="tab-settings" md-label="Settings" to="/components/tabs/settings"></md-tab>
+      <md-tab id="tab-disabled" md-label="Disabled" md-disabled></md-tab>
+    </md-tabs>
     <router-view/>
   </div>
 </template>
@@ -11,13 +17,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+  export default {
+    name: 'TabRouter'
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 
