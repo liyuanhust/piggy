0612160ef0d459070b7d8dee8db327f922f3f351
Jiayang Yan <raymond.dream@gmail.com>
2018-03-17 21:26:39 +0800

    [YJY] add confirm button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <md-field>
       <label>猪ID</label>
       <md-input v-model="pigId"></md-input>
@@ -8,8 +8,16 @@
       <label>饲养场</label>
       <md-input v-model="farm"></md-input>
     </md-field>
+    <md-button class="md-raised md-primary">提交</md-button>
   </div>
 </template>
+
+<style>
+ .container {
+   width: 70%;
+   margin: 0 auto;
+ }
+</style>
 
 <script>
   export default {
